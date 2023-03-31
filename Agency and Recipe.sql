3c41e36732eed1164fd4b496571eef23c409c230
BehuzCoderr <BehruzMulkamonov@gmail.com>
2023-03-31 20:16:20 +0500

    About agency and recipe

---
+++ Agency and Recipe.sql
@@ -0,0 +1,17 @@
+--Agency
+
+--ex_1
+-- SELECT agtfirstname, agtlastname, agtphonenumber from agents
+--ex_2
+-- SELECT * from engagements
+--ex_3
+-- SELECT * from engagements order by startdate asc
+--SELECT * from engagements order by startdate desc
+
+--Recipe
+
+--ex_1
+-- SELECT * from ingredients
+--ex_2
+select 	Recipetitle from recipes
+order by recipetitle asc
